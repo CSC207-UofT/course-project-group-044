523a6ac4ba32ea5d7091130a686f434d6fbceb01
duankunh <2446925718@qq.com>
2021-10-15 21:46:05 -0400

    controller part with some bugs, someone please come to assist

---
+++ src/main/java/Commandline.java
@@ -0,0 +1,11 @@
+
+public class Commandline {
+
+    public static void main(String[] args) {
+
+        Managermentsystem em = new Managermentsystem();
+        SystemInOut inOut = new SystemInOut();
+        em.run(inOut);
+
+    }
+}
