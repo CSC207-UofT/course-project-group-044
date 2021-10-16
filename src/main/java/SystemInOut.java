7bbac4044323e7007ed74b39385b1e48ade65176
CNJasonChen <jasonjiawei.chen@mail.utoronto.ca>
2021-10-15 22:01:38 -0400

    add the command line

--- src/main/java/SystemInOut.java
+++ src/main/java/SystemInOut.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class SystemInOut implements Managermentsystem.InOut {
+public class SystemInOut implements ManagementSystem.InOut {
 
     BufferedReader reader;
 
