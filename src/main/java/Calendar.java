04b6035c7e8710bb5c36b86550aaf37991a0d562
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2021-10-29 14:04:55 -0400

    calendar: Add queries useful to scheduling
    Add two methods to the Calendar class querying the events. This expose a
    simple high-level interface to the scheduler.
    
    Signed-off-by: Alyssa Rosenzweig <a.rosenzweig@mail.utoronto.ca>

--- src/main/java/Calendar.java
+++ src/main/java/Calendar.java
@@ -1,4 +1,4 @@
-import java.time.Instant;
+import java.time.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
@@ -12,6 +12,30 @@ public class Calendar {
 
     public void addEvent(Event event) {
         this.events.add(event);
+    }
+
+    /**
+     * Get the number of events on a given day.
+     *
+     * @param Date/time to check
+     * @return Count
+     */
+    public long eventsOnDay(ZonedDateTime argument) {
+        LocalDate ref = argument.toLocalDate();
+        ZoneId zone = argument.getZone();
+
+        return events.stream().filter(c -> c.getDate(zone).equals(ref)).count();
+    }
+
+    /**
+     * Get the number of hours scheduled in a given week.
+     *
+     * @param Date/time to check
+     * @return Count
+     */
+    public long hoursInWeek(ZonedDateTime argument) {
+        return events.stream().filter(c -> c.isSameWeek(argument))
+                     .mapToLong(c -> c.getHours()).sum();
     }
 
     public void delEvent(){
