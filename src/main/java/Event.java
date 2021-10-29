44a1631d7fc5fc29189bb72e56e64cdbae3a5e7f
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2021-10-29 14:18:58 -0400

    event: Add queries
    Add some query methods for properties of the Event. These will be
    useful for scheduling.
    
    Signed-off-by: Alyssa Rosenzweig <a.rosenzweig@mail.utoronto.ca>

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,5 +1,5 @@
-import java.time.Duration;
-import java.time.Instant;
+import java.time.*;
+import java.time.temporal.ChronoField;
 
 public abstract class Event {
 	private Instant start;
@@ -13,4 +13,37 @@ public abstract class Event {
         this.name = name;
         this.location = location;
 	}
+
+    /**
+     * Get the date of a shift in a given timezone
+     *
+     * @return Local date
+     */
+    public LocalDate getDate(ZoneId zone) {
+            return start.atZone(zone).toLocalDate();
+    }
+
+    /**
+     * Test if the event occur during the same week as a given date/time.
+     *
+     * @return True if so.
+     */
+    public boolean isSameWeek(ZonedDateTime ref) {
+        ZonedDateTime comp = ZonedDateTime.ofInstant(start, ZoneOffset.UTC); // TODO
+
+        int week_1 = ref.get(ChronoField.ALIGNED_WEEK_OF_YEAR);
+        int week_2 = comp.get(ChronoField.ALIGNED_WEEK_OF_YEAR);
+
+        return ref.getYear() == comp.getYear() && week_1 == week_2;
+    }
+
+    /**
+     * Get the number of hours spanned by the event
+     *
+     * @return Hours
+     */
+    public long getHours() {
+        return duration.toHours();
+    }
+
 }
