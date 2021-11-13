a6f44533dd8fdc76fbbd53ed6c6b67ce0b477791
oliviachen <lovelyolivia.chen@mail.utoronto.ca>
2021-11-12 21:06:06 -0500

    doc

--- src/main/java/Entity/Meeting.java
+++ src/main/java/Entity/Meeting.java
@@ -10,6 +10,13 @@ import java.time.Instant;
 import java.util.List;
 
 @Entity
+/**
+ * An entity class that stores the information of Meeting. The superclass is Event.
+ *
+ * @param holder        the event holder of this Meeting.
+ * @param participants  the list of Employees attended this Meeting.
+ * @see Event
+ */
 public class Meeting extends Event {
     @Column
     private Employee holder;
@@ -17,6 +24,14 @@ public class Meeting extends Event {
     @Column
     private List<Employee> participants;
 
+    /**
+     * Constructor to create an instance of Meeting that has full complete information .
+     *
+     *  @param start        the start time of this Event.
+     *  @param duration     the amount of time that from Event start to end.
+     *  @param name         the name of this Event.
+     *  @param location     the specific address of this Event.
+     */
 	public Meeting(Employee holder, List<Employee> participants, Instant start,
                    Duration duration, String name, String location) {
         super(start, duration, name, location);
