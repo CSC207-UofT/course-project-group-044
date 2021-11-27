b61d0223ffa0db996a1995f581a66997598ba117
dkhhandsome <2446925718@qq.com>
2021-11-27 00:01:09 -0500

    List of changes are made: + add hirepage.css file to make a better feature of hr system + add div style into each div in hr system html file to make a better feature. + add static package to storage css and js file - delete original css page since it is not working out

---
+++ src/main/java/com/hr/controller/ShiftController.java
@@ -0,0 +1,88 @@
+package com.hr.controller;
+
+import com.hr.entity.Employee;
+import com.hr.service.Scheduler;
+import com.hr.service.impl.SchedulerImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import com.hr.service.EmployeeModifier;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.LocalDateTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+@RequestMapping("employee")
+public class ShiftController {
+    private static Employee DUMMY = new Employee();
+
+    @Autowired
+    private SchedulerImpl scheduler;
+    @Autowired
+    private EmployeeModifier employeeModifier;
+
+    public ShiftController(){}
+
+    @PostMapping("/addshift")
+    public String addShift(@ModelAttribute(value="employee")Employee employee, String date, String location,
+                           Integer hours){
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
+        LocalDateTime dateTime = LocalDateTime.parse(date, formatter);
+
+        ZoneId zoneId = ZoneId.of( "Asia/Kolkata" );        //Zone information
+
+        ZonedDateTime zdtAtAsia = dateTime.atZone( zoneId );     //Local time in Asia timezone
+
+        Employee user = employeeModifier.findEmployeeById(employee.getId());
+
+        if (user != null) {scheduler.scheduleShift(user, zdtAtAsia, location, hours);
+            return "hirepage";}
+        return "hirepage";
+    }
+    @PostMapping("/addmeeting")
+    public String addMeeting(@ModelAttribute(value="employee")Employee employee, List<Integer> participants, String date,
+                           String name, String location, Integer hours){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
+        LocalDateTime dateTime = LocalDateTime.parse(date, formatter);
+
+        ZoneId zoneId = ZoneId.of( "Asia/Kolkata" );        //Zone information
+
+        ZonedDateTime zdtAtAsia = dateTime.atZone( zoneId );     //Local time in Asia timezone
+        Employee user = employeeModifier.findEmployeeById(employee.getId());
+        ArrayList<Employee> guests= new ArrayList<>();
+        for (Integer e: participants){
+            Employee guest = employeeModifier.findEmployeeById(e); //"later on I will add "guest not found function"
+            guests.add(guest);
+        }
+        if (user != null) {
+            scheduler.scheduleMeeting(user, guests, zdtAtAsia, name, location, hours);
+        return "hirepage";}
+        return "hirepage";
+    }
+
+
+    @PostMapping("/removeshift")
+    public String removeShift(@ModelAttribute(value="employee")Employee employee, ZonedDateTime date, String location, Integer hours){
+        Employee user = employeeModifier.findEmployeeById(employee.getId());
+        if (user != null) {scheduler.cancelShift(user, date, location, hours);
+        return "hirepage";}
+        return "hirepage";
+    }
+    @PostMapping("/removemeeting")
+    public String removeMeeting(@ModelAttribute(value="employee")Employee employee, List<Employee> participants, ZonedDateTime date,
+                              String name, String location, Integer hours){
+        Employee user = employeeModifier.findEmployeeById(employee.getId());
+        if (user != null){scheduler.cancelMeeting(user, participants, date, location, name, hours);
+        return "hirepage";}
+        return "hirepage";
+    }
+}
