400a2f0e0063c1dc0f3a26e3e6f80142ce3e3685
dkhhandsome <D2446925718Kh@gmail.com>
2021-12-04 19:05:53 -0500

    + SecurityController to enable user access to all info + deleting original login and replacing it with spring boot security.

--- src/main/java/com/hr/controller/SecurityController.java
+++ src/main/java/com/hr/controller/SecurityController.java
@@ -1,7 +1,5 @@
 package com.hr.controller;
 
-
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
