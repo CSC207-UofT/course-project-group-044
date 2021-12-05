835f04f6057d27d2bc14c9d2ab16b97e8e2dcdf5
oliviachen <lovelyolivia.chen@mail.utoronto.ca>
2021-12-05 17:08:40 -0500

    javadoc

--- src/main/java/com/hr/controller/SecurityController.java
+++ src/main/java/com/hr/controller/SecurityController.java
@@ -7,10 +7,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
+/**
+ * Controller for providing the web based security configuration.
+ *  - Require the user to be authenticated prior to accessing any URL within our application
+ *  - Create a user with the username “user”, password “password”, and role of “USER”
+ *  - automatically render a login page and logout success page
+ *
+ */
 public class SecurityController extends WebSecurityConfigurerAdapter {
 
 
     @Override
+    /**
+     * Grant to access the database without login as user, while in other case web security controller still work.
+     */
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/h2", "/h2/*");
     }
