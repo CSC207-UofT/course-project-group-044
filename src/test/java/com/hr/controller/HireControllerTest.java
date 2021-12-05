2902ec40d904e00372f6d7eb39f3c25e9e269bb5
dkhhandsome <D2446925718Kh@gmail.com>
2021-12-05 14:06:31 -0500

    + fix bug

--- src/test/java/com/hr/controller/HireControllerTest.java
+++ src/test/java/com/hr/controller/HireControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -23,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(HireController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class HireControllerTest {
     private static final String DUMMY_ID = "1";
 
--- src/test/java/com/hr/controller/WelcomeControllerTest.java
+++ src/test/java/com/hr/controller/WelcomeControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(WelcomeController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class WelcomeControllerTest {
     private static final String DUMMY_ID = "1";
 
